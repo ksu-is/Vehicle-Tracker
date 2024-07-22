d0062d1625b5b64490dbd4a4800c0a15a4144e7c
Julicia Brown <jbrow892@students.kennesaw.edu>
2024-07-21 23:23:41 -0400

    Add all stages and menu

--- vehicle-tracker.py
+++ vehicle-tracker.py
@@ -2,6 +2,8 @@ import os
 import platform 
 import tkinter as tk
 import random
+from functools import partial
+from itertools import ifilter
 from tkinter import ttk
 from tkinter import messagebox
 
@@ -22,12 +24,47 @@ def add_vehicle():
     print(vehicle_list)
 
 def remove_vehicle():
-    global vehicle_list
-    removal = input("Do you know the ID for the Vehicle to remove?")
+    global vehicle_list2
+    
+    removal = input("Do you know the ID for the Vehicle to remove? ")
     if removal == "no":
             print(vehicle_list)
             removal = input("Input Id for vehicle:")
     else:
-        vehicle_list.remove(removal)
+        Id_search = [word for word in vehicle_list if word in removal]
+        print(Id_search)
+        vehicle_list.remove(new_ID)
+    
 
-remove_vehicle()
+    print(vehicle_list)
+
+
+def print_inventory():
+     global vehicle_list
+     print(vehicle_list)
+
+def opening_menu():
+     print("Welcome to J&J Car Tracker! What would you like to do?")
+     menu = {}
+     menu ['1'] = "Add Vehicle"
+     menu ['2'] = "Remove Vehicle"
+     menu ['3'] = "Find Vehicle ID"
+     menu ['4'] = "Exit"
+     while True: 
+        options=menu.keys()
+        for entry in options: 
+             print (entry, menu[entry])
+
+        selection = input("Please Select:") 
+        if selection =='1': 
+            add_vehicle() 
+        elif selection == '2': 
+            remove_vehicle()
+        elif selection == '3':
+            print_inventory()
+        elif selection == '4': 
+            break
+        else: 
+            print ("Unknown Option Selected!")
+
+opening_menu()
