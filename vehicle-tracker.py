6c1f028d45c00a0105b3c0137d709bc267d09742
Julicia Brown <jbrow892@students.kennesaw.edu>
2024-07-21 21:31:49 -0400

    Add Vehicle

--- vehicle-tracker.py
+++ vehicle-tracker.py
@@ -4,5 +4,13 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
+vehicle_list = []
+
 def add_vehicle(): 
-    print("What is the make and model of the vehicle?")
+    print("What is the Year, Make and Model of the vehicle?")
+    v_year = input("Year:")
+    v_make = input("Make:")
+    v_model = input("model:")
+    vehicle = v_year + v_make + v_model
+    vehicle_list = vehicle_list.append(vehicle)
+    
